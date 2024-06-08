440dac175859430fedd60e93bdc635482e572caf
Maxx <maxx@aolc.ru>
2024-06-08 19:19:30 +0700

    add-docker: begin

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,50 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.Valid;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+    private final UserClient userClient;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> create(@Valid @RequestBody UserDto userDto) {
+        return userClient.createUser(userDto);
+
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> findById(@PathVariable Long userId) {
+        return userClient.getUser(userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {
+        return userClient.getUsers();
+    }
+
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> update(@Valid @RequestBody UserDto userDto,
+                          @PathVariable Long userId) {
+        return userClient.patchUser(userId, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remove(@PathVariable Long userId) {
+        userClient.deleteUser(userId);
+    }
+}
